ea21730a0d768d73e00c388c4a0787b43a752452
Nemesix493 <verquerre.kevin@gmail.com>
2025-02-18 16:31:12 +0100

    feat(image-size): add extend fucntions

--- os_image_prepper/image_size.py
+++ os_image_prepper/image_size.py
@@ -1,5 +1,7 @@
 from logging import getLogger
+from pathlib import Path
 import subprocess
+import time
 
 
 logger = getLogger("GlobalLogger")
@@ -12,4 +14,36 @@ def extend_image(image_path, size_mo):
         logger.info(f"✅ {size_mo} Mo added to : {image_path}")
     except subprocess.CalledProcessError as e:
         logger.error(f"❌ Adding {size_mo} Mo failed due to this error :\n {e}")
+        exit(1)
+
+
+def extend_root_partition(image_path):
+    logger.info(f"🔄 Extending root partition  : {image_path}")
+    try:
+        loop_dev = subprocess.run(["losetup", "-Pf", "--show", image_path], check=True, text=True)
+        logger.debug(f"losetup -Pf --show $IMAGE_PATH -> {loop_dev.stdout}")
+        time.sleep(2)
+        subprocess.run(["parted", "--script", loop_dev.stdout, "resizepart", "2", "100%"])
+        subprocess.run(["losetup", "-d", loop_dev.stdout])
+        logger.info("✅ root partition extended")
+    except subprocess.CalledProcessError as e:
+        subprocess.run(["losetup", "-d", loop_dev.stdout])
+        logger.error(f"❌ root partition extention failed due to this error :\n {e}")
+        exit(1)
+
+
+def extend_root_fs(image_path):
+    try:
+        loop_dev = subprocess.run(["losetup", "-Pf", "--show", image_path], check=True, text=True)
+        logger.debug(f"losetup -Pf --show $IMAGE_PATH -> {loop_dev.stdout}")
+        subprocess.run(["kpartx", "-av", loop_dev.stdout], check=True)
+        time.sleep(2)
+        root_partition = Path(f"/dev/mapper/{Path(loop_dev.stdout).name}p2")
+        logger.debug(f"root_partition -> {root_partition}")
+        subprocess.run(["e2fsck", "-f", root_partition], check=True, text=True).stdout
+        subprocess.run(["resize2fs", root_partition], check=True, text=True).stdout
+        subprocess.run(["kpartx", "-d", loop_dev.stdout], check=True)
+        subprocess.run(["losetup", "-d", loop_dev.stdout], check=True)
+    except subprocess.CalledProcessError as e:
+        logger.error(f"❌ root partition extention failed due to this error :\n {e}")
         exit(1)
